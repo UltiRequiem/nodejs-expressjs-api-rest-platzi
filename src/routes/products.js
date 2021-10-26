778506bda70faff9e8a57f91d1903eec64dbd009
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-10-26 05:44:11 -0500

    :sparkles: Query

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,8 +2,14 @@ import { Router } from 'express';
 
 const productsRouter = Router();
 
-productsRouter.get('/', (_request, response) => {
-  response.json([{ product: 'watermelon' }]);
+productsRouter.get('/', ({ query }, response) => {
+  const data = [
+    { product: 'watermelon' },
+    { product: 'apple' },
+    { product: 'apple pie' },
+  ];
+
+  response.json(query.limit ? data.slice(0, query.limit) : data);
 });
 
 productsRouter.get('/:id', ({ params }, response) => {
