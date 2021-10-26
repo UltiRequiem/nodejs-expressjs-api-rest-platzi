1d8f9ab5f4624a4d75537448301ded233b7e1472
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-10-26 07:44:41 -0500

    :wrench: Add Vercel Config

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # Express.js Course
 
 ![Cover](./cover.png)
+
+Course link: https://platzi.com/clases/backend-nodejs
+
+Deploy:
--- src/routes/v1/products.js
+++ src/routes/v1/products.js
@@ -16,4 +16,8 @@ productsRouter.post('/', ({ body }, response) => {
   response.json({ message: 'Successfully posted.', body });
 });
 
+productsRouter.patch('/:id', ({ body, params }, response) => {
+  response.json({ message: 'Successfully posted.', id: params.id, body });
+});
+
 export default productsRouter;
