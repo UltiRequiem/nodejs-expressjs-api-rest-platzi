3a18e9cf4e32f1651ebca4ce2b3271ccd43486ae
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-10-26 06:19:55 -0500

    :recycle: Refactoring Utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,12 @@
 import faker from 'faker';
 
 export function generateProductsData(quantity) {
-  const products = [];
-
-  // eslint-disable-next-line no-plusplus
-  for (let index = 0; index < quantity; index++) {
-    products.push({
-      name: faker.commerce.product(),
-      id: index,
-      price: `$${faker.commerce.price()}`,
-      image: faker.image.imageUrl(),
-    });
-  }
-
-  return products;
+  return Array.from({ length: quantity }).map((_element, index) => ({
+    name: faker.commerce.product(),
+    id: index,
+    price: `$${faker.commerce.price()}`,
+    image: faker.image.imageUrl(),
+  }));
 }
 
 export const defaultData = generateProductsData(100);
