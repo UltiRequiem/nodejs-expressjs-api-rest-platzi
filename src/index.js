0665c97ab9e9b2ba5761fed24fbd37c4d6ce9e68
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-10-27 22:14:03 -0500

    :sparkles: Update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express, { json } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 
 import { PORT } from './config.js';
 
@@ -13,6 +15,17 @@ import {
 const app = express();
 
 app.use(json());
+app.use(helmet());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // eslint-disable-next-line no-unused-expressions
+      [`http://localhost:${PORT}`].includes(origin)
+        ? callback(undefined, true)
+        : callback(new Error('Not allowed by CORS!'));
+    },
+  }),
+);
 
 app.use('/v1', routerV1());
 
