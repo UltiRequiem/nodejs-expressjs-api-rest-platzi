14153b355f4d1358db769ce305c4d0fbc5b10de1
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-14 16:19:14 -0500

    :sparkles: Models

---
+++ src/db/models/user.model.js
@@ -0,0 +1,44 @@
+import { Model, DataTypes, Sequelize } from 'sequelize';
+
+const USER_TABLE = 'users';
+
+const UserSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    unique: true,
+  },
+  password: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'create_at',
+    defaultValue: Sequelize.NOW,
+  },
+};
+
+class User extends Model {
+  static associate() {
+    // associate
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: USER_TABLE,
+      modelName: 'User',
+      timestamps: false,
+    };
+  }
+}
+
+export { USER_TABLE, UserSchema, User };
