4d3e242acde293b424dc3c4f83d75d7df216df93
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-21 15:32:10 -0500

    :recycle: Refactoring Connections

--- src/config.js
+++ src/config.js
@@ -7,4 +7,11 @@ export const {
   ENV, DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT,
 } = process.env;
 
+export const [USER, PASSWORD] = [
+  encodeURIComponent(DB_USER),
+  encodeURIComponent(DB_PASSWORD),
+];
+
+export const URI = `postgres://${USER}:${PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
 export const PORT = process.env.PORT || 3000;
--- src/libs/sequelize.js
+++ src/libs/sequelize.js
@@ -2,17 +2,11 @@ import { Sequelize } from 'sequelize';
 
 import setupModels from '../db/models/module.js';
 
-import {
-  DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT,
-} from '../config.js';
-
-const USER = encodeURIComponent(DB_USER);
-const PASSWORD = encodeURIComponent(DB_PASSWORD);
-const URI = `postgres://${USER}:${PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+import { URI } from '../config.js';
 
 const sequelize = new Sequelize(URI, {
   dialect: 'postgres',
-  logging: false,
+  logging: true,
 });
 
 setupModels(sequelize);
